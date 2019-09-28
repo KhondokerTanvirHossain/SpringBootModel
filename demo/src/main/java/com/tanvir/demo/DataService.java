e6281e9ff1aa88fee2a9bc1f0be2b358aedc8ad9
Tanvir Hossain <k.tanvir.hossain@gmail.com>
2019-09-29 04:40:55 +0600

    SpringBootModel with apache embeded Database
    use DataRepository to take action on Database
    Keep old (with out external Database) code as comment

--- demo/src/main/java/com/tanvir/demo/DataService.java
+++ demo/src/main/java/com/tanvir/demo/DataService.java
@@ -1,5 +1,6 @@
 package com.tanvir.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -8,67 +9,93 @@ import java.util.Arrays;
 @Service
 public class DataService {
 
-    private ArrayList<DataModel> list = new ArrayList<DataModel>(Arrays.asList(
-            new DataModel(1,"name1", "job1"),
-            new DataModel(2,"name2", "job2"),
-            new DataModel(3, "name3", "job3")
-    ));
+//    private ArrayList<DataModel> list = new ArrayList<DataModel>(Arrays.asList(
+//            new DataModel(1,"name1", "job1"),
+//            new DataModel(2,"name2", "job2"),
+//            new DataModel(3, "name3", "job3")
+//    ));
+
+    @Autowired
+    private DataRepository dataRepository;
+
     public ArrayList<DataModel> getAll(){
-        return list;
+        ArrayList<DataModel> dataModels = new ArrayList<DataModel>();
+        dataRepository.findAll().forEach(dataModels::add);
+        return dataModels;
     }
     public DataModel getOne(int id){
-        return list.stream()
-                .filter((dataModel) -> dataModel.getId() == id)
-                .findFirst()
-                .get();
+        return dataRepository.
+                findById(id).
+                get();
+//        return list.stream()
+//                .filter((dataModel) -> dataModel.getId() == id)
+//                .findFirst()
+//                .get();
     }
     public String getName(int id){
-        return list.stream()
-                .filter((dataModel -> dataModel.getId() == id))
-                .findFirst()
+        return dataRepository
+                .findById(id)
                 .get()
                 .getName();
+//        return list.stream()
+//                .filter((dataModel -> dataModel.getId() == id))
+//                .findFirst()
+//                .get()
+//                .getName();
     }
 
     public String getJobDescription(int id){
-        return list.stream()
-                .filter((dataModel -> dataModel.getId() == id))
-                .findFirst()
+        return dataRepository
+                .findById(id)
                 .get()
                 .getJobDescription();
-    }
-    public void updateOne(DataModel model){
-        DataModel data = list.stream()
-                .filter((dataModel -> dataModel.getId() == model.getId()))
-                .findFirst()
-                .get();
-        data.setName(model.getName());
-        data.setJobDescription(model.getJobDescription());
+//        return list.stream()
+//                .filter((dataModel -> dataModel.getId() == id))
+//                .findFirst()
+//                .get()
+//                .getJobDescription();
     }
 
     public void addOne(DataModel model){
-        list.add(model);
+        dataRepository.save(model);
+//        list.add(model);
+    }
+
+    public void updateOne(DataModel model){
+        dataRepository.save(model);
+//        DataModel data = list.stream()
+//                .filter((dataModel -> dataModel.getId() == model.getId()))
+//                .findFirst()
+//                .get();
+//        data.setName(model.getName());
+//        data.setJobDescription(model.getJobDescription());
     }
 
     public void updateName(int id, String name){
-        DataModel data = list.stream()
-                .filter((dataModel -> dataModel.getId() == id))
-                .findFirst()
-                .get();
-        data.setName(name);
+        DataModel model = dataRepository.findById(id).get();
+        dataRepository.save(new DataModel(id, name, model.getJobDescription()));
+//        DataModel data = list.stream()
+//                .filter((dataModel -> dataModel.getId() == id))
+//                .findFirst()
+//                .get();
+//        data.setName(name);
     }
 
     public void updateJobDescription(int id, String jobDescription){
-        DataModel data = list.stream()
-                .filter((dataModel -> dataModel.getId() == id))
-                .findFirst()
-                .get();
-        data.setJobDescription(jobDescription);
+        DataModel model = dataRepository.findById(id).get();
+        dataRepository.save(new DataModel(id, model.getName(), jobDescription));
+//        DataModel data = list.stream()
+//                .filter((dataModel -> dataModel.getId() == id))
+//                .findFirst()
+//                .get();
+//        data.setJobDescription(jobDescription);
     }
     public void deleteOne(DataModel model){
-        list.removeIf(dataModel -> dataModel.getId() == model.getId());
+        dataRepository.delete(model);
+//        list.removeIf(dataModel -> dataModel.getId() == model.getId());
     }
     public void deleteById(int id){
-        list.removeIf(dataModel -> dataModel.getId() == id);
+        dataRepository.delete(dataRepository.findById(id).get());
+//        list.removeIf(dataModel -> dataModel.getId() == id);
     }
 }
