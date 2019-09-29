fd354821c3bda9530760943e66926648c6d38483
Tanvir Hossain <k.tanvir.hossain@gmail.com>
2019-09-29 07:19:39 +0600

    with mysql server
    only @GeneratedValue added

--- demo/src/main/java/com/tanvir/demo/DataModel.java
+++ demo/src/main/java/com/tanvir/demo/DataModel.java
@@ -1,11 +1,14 @@
 package com.tanvir.demo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class DataModel {
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
     private String name;
     private String jobDescription;
